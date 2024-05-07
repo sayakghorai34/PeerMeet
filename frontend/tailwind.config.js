efa0b9bc65080ab2f2f1777c51c93fc6447541d9
Sayak Ghorai <sayakghorai@Sayaks-MacBook-Air-M1.local>
2024-05-07 22:05:00 +0530

    Tailwind Deprication Fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  media: false, // or 'media' or 'class'
   mode: "jit",
   theme: {
     colors: {
